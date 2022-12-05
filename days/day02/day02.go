60b7e8455887a38997c7b52781753788c9be6235
saulomarx <saulo.paiva@contaquanto.com.br>
2022-12-05 11:29:25 -0300

    Day 02     * adds main day 02     * solves day 02

--- days/day02/day02.go
+++ days/day02/day02.go
@@ -1,5 +1,98 @@
 package day02
 
-func Part01() {}
+import "fmt"
 
-func Part02() {}
+const r = "ROCK"
+const p = "PAPER"
+const s = "SCISSOR"
+
+const w = "WIN"
+const l = "LOSE"
+const d = "DRAW"
+
+var winPoints = map[string]int{r: 1, p: 2, s: 3}
+var strategy = map[string]string{"X": l, "Y": d, "Z": w}
+var winStrategy = map[string]string{p: s, s: r, r: p}
+var loseStrategy = map[string]string{s: p, r: s, p: r}
+var playMap = map[string]string{"A": r, "X": r, "B": p, "Y": p, "C": s, "Z": s}
+
+func Part01(in []string) {
+	fmt.Println("What would your total score be if everything goes exactly according to your strategy guide?")
+	var roundPoints int
+	for _, game := range in {
+		o := playMap[string(game[0])]
+		m := playMap[string(game[2])]
+		roundPoints = roundPoints + resolveGame(o, m)
+	}
+	fmt.Println(roundPoints)
+
+}
+
+func Part02(in []string) {
+	fmt.Println("Following the Elf's instructions for the second column, what would your total score be if everything goes exactly according to your strategy guide?")
+	var roundPoints int
+	for _, game := range in {
+		o := string(game[0])
+		m := string(game[2])
+
+		o, m = changePlay(o, m)
+
+		roundPoints = roundPoints + resolveGame(o, m)
+	}
+	fmt.Println(roundPoints)
+
+}
+
+func resolveGame(o, m string) int {
+	roundPoints := winPoints[m]
+
+	if o != m {
+		// I play rock
+		if m == r {
+			if o == p {
+				return roundPoints
+			}
+			return roundPoints + 6
+		}
+
+		// I play Scissor
+		if m == s {
+			if o == r {
+				return roundPoints
+			}
+			return roundPoints + 6
+		}
+
+		// I play paper
+		if m == p {
+			if o == s {
+				return roundPoints
+			}
+			return roundPoints + 6
+		}
+	}
+
+	// Same play
+	return roundPoints + 3
+}
+
+func changePlay(o, m string) (string, string) {
+	result := strategy[m]
+
+	o = playMap[o]
+
+	if result == w {
+		m = winStrategy[o]
+	}
+
+	if result == l {
+		m = loseStrategy[o]
+	}
+
+	if result == d {
+		m = o
+	}
+
+	return o, m
+
+}
--- main.go
+++ main.go
@@ -4,15 +4,18 @@ import (
 	"fmt"
 
 	"github.com/saulomarx/AoC2022/days/day01"
+	"github.com/saulomarx/AoC2022/days/day02"
 	"github.com/saulomarx/AoC2022/utils"
 )
 
 func main() {
-	workingDay := 1
+	workingDay := 2
 
 	switch workingDay {
 	case 1:
 		runsDay01()
+	case 2:
+		runsDay02()
 	default:
 		fmt.Println("not today")
 	}
@@ -26,52 +29,9 @@ func runsDay01() {
 	day01.Part02(input)
 }
 
-// func main2() {
-// 	inputs := readInput()
-// 	messure := convertInputToInt(inputs)
-
-// 	var howDeepWeGo = 0
-// 	for i, el := range messure {
-// 		if i != 0 {
-// 			if el > messure[i-1] {
-// 				howDeepWeGo++
-// 			}
-// 		}
-// 	}
-// 	fmt.Println(howDeepWeGo)
-
-// }
-
-// func readInput() []string {
-// 	var input []string
-// 	file, err := os.Open("./inputs/day01/input.txt")
-// 	if err != nil {
-// 		fmt.Println("Deu ruim")
-// 	}
-// 	leitor := bufio.NewReader(file)
-
-// 	for {
-// 		line, err := leitor.ReadString('\n')
-// 		if err == io.EOF {
-// 			break
-// 		}
-// 		line = strings.TrimSpace(line)
-
-// 		input = append(input, line)
-// 	}
-
-// 	return input
-// }
-
-// func convertInputToInt(input []string) []int {
-// 	var response []int
-// 	for _, el := range input {
-// 		value, err := strconv.Atoi(el)
-
-// 		if err == nil {
-// 			response = append(response, value)
-// 		}
-
-// 	}
-// 	return response
-// }
+func runsDay02() {
+	p := "./days/day02/inputs/in01.txt"
+	input := utils.ReadLines(p)
+	day02.Part01(input)
+	day02.Part02(input)
+}
